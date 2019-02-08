adc2e7c1110631a311d65e2436b697cf582f6b89
Manuel Govea <manumanu-96@hotmail.com>
2019-02-08 15:29:38 -0500

    removed gitignored files

---
+++ ObjCreationTest/Assets/resources/objectData/data/objectData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName = "objectData", menuName ="FlowObject")]
+public class ObjectData : ScriptableObject {
+
+    public GameObject prefab;
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 scale;
+    public string objectName;
+
+}
---
+++ ObjCreationTest/Assets/scripts/ObjectManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class ObjectManager : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
