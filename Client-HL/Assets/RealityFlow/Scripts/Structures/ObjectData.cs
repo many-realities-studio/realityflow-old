dbb4c8dfe9acad71edfe343b585bb8d93533699e
jon <jon@DESKTOP-PJNFE6K.localdomain>
2019-03-25 18:58:26 -0400

    Resolved more merge conflict errors that were not detected by git

--- Client-HL/Assets/RealityFlow/Scripts/Structures/ObjectData.cs
+++ Client-HL/Assets/RealityFlow/Scripts/Structures/ObjectData.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿// TODO: encapsulate fields?
+// TODO: encapsulate fields?
 
 [System.Serializable]
 public class ObjectData
@@ -11,17 +10,3 @@ public class ObjectData
     public string scale;
     public string name;
 }
-=======
-﻿// TODO: encapsulate fields?
-
-[System.Serializable]
-public class ObjectData
-{
-
-    public string prefab;
-    public string position;
-    public string rotation;
-    public string scale;
-    public string name;
-}
->>>>>>> 8592be32e56dfeab7e1fa58ed0fe1b8046b73170
--- Client-HL/Assets/RealityFlow/Scripts/UI/MovePanel.cs
+++ Client-HL/Assets/RealityFlow/Scripts/UI/MovePanel.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -33,39 +32,3 @@ public class MovePanel : MonoBehaviour {
         }
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MovePanel : MonoBehaviour {
-
-    public const float PANEL_OFFSET = 737f;
-
-    // Moves a panel a given number of screen-dependent pixels in a given direction
-    // panel = -1 refers to the scene (left) panel
-    // panel = 1 refers to the project (right) panel
-    public void Move (int panel)
-    {
-        if ( (GetComponent<RectTransform>().offsetMin.x == PANEL_OFFSET * panel) || 
-            (GetComponent<RectTransform>().offsetMax.x == PANEL_OFFSET * panel) )
-        {
-            // Set left, bottom RectTransform properties of panel
-            GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
-            // Set right, top RectTransform properties of panel
-            GetComponent<RectTransform>().offsetMax = new Vector2(0, 0);
-            // Bring panel to foreground
-            GetComponent<RectTransform>().SetAsLastSibling();
-        }
-        else
-        {
-            // Set left, bottom RectTransform properties of panel
-            GetComponent<RectTransform>().offsetMin = new Vector2(GetComponent<RectTransform>().offsetMin.x + PANEL_OFFSET * panel, 0);
-            // Set right, top RectTransform properties of panel
-            GetComponent<RectTransform>().offsetMax = new Vector2(GetComponent<RectTransform>().offsetMax.x + PANEL_OFFSET * panel, 0);
-            // Send panel to background
-            GetComponent<RectTransform>().SetAsFirstSibling();
-        }
-    }
-}
->>>>>>> 8592be32e56dfeab7e1fa58ed0fe1b8046b73170
