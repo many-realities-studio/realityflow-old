c1a39bfa5c4c71c07b6a7509daa059c9150e75d3
JonCWhite <36213429+JonCWhite@users.noreply.github.com>
2019-04-03 11:19:18 -0400

    Added registration and project selection
    This push adds a UI for registering, logging in, and selecting a project. It also includes a json with data to test the project load process.

---
+++ Client-HL/Assets/RealityFlow/Scripts/Structures/ProjectListItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectListItem : MonoBehaviour {
+
+    public string name;
+    public string id;
+    //public GameObject obj;
+    public int index;
+    public ProjectListManager manager;
+
+    public void select()
+    {
+        manager.select(this);
+    }
+}
