b1d600953ec7ad06a4991f66aa1e29ebfb626e0a
Yash <bhosaley5@gmail.com>
2020-02-20 01:06:46 -0500

    moving my database code into the FAM database file, as well as some minor changes

--- src/FastAccessStateTracker/Database/IDatabase.ts
+++ src/FastAccessStateTracker/Database/IDatabase.ts
@@ -1,6 +1,7 @@
 import { FlowProject } from "../FlowLibrary/FlowProject";
 import { FlowUser } from "../FlowLibrary/FlowUser";
 import { FlowObject } from "../FlowLibrary/FlowObject";
+import {FlowClient} from "../FlowLibrary/FlowClient";
 
 /**
  * IDatabase defines all of the necessary methods needed to connect an arbitrary database to the system.
@@ -8,21 +9,21 @@ import { FlowObject } from "../FlowLibrary/FlowObject";
  */
 export interface IDatabase
 { 
-  // Project functions
-  CreateProject(projectToCreate : FlowProject) : boolean;
-  DeleteProject(projectToDelete : FlowProject) : boolean;
-  UpdateProject(projectToUpdate : FlowProject) : boolean;
-  GetProject(projectId : number) : FlowProject;
+// Project functions
+CreateProject(projectToCreate: FlowProject, projectUser: FlowUser, projectClient: FlowClient) : void;
+DeleteProject(projectToDelete : FlowProject) : void;
+UpdateProject(projectToUpdate : FlowProject) : void;
+GetProject(projectId : number) : Promise<FlowProject>;
 
-  // User functions
-  CreateUser(userToCreate : FlowUser) : boolean;
-  DeleteUser(userToDelete : FlowUser) : boolean;
-  UpdateUser(userToUpdate : FlowUser) : boolean;
-  GetUser(UserId : number) : FlowUser;
+// User functions
+CreateUser(userToCreate: FlowUser, userClient: FlowClient): void;
+DeleteUser(userToDelete : FlowUser) : void;
+UpdateUser(userToUpdate : FlowUser) : void;
+GetUser(UserId: number): Promise<FlowUser> ;
 
-  // Object functions
-  CreateObject(objectToCreate : FlowObject) : boolean;
-  DeleteObject(objectToDelete : FlowObject) : boolean;
-  UpdateObject(objectToUpdate : FlowObject) : boolean;
-  GetObject(ObjectId : number) : FlowObject;
+// Object functions
+CreateObject(objectToCreate: FlowObject, objectProject: FlowProject): Promise<Object>;
+DeleteObject(projectToDelete : FlowProject, projectObject:FlowObject): Promise<void>;
+UpdateObject(objectToUpdate : FlowObject) : void;
+GetObject(ObjectId : number): Promise<FlowObject>;
 }
--- src/FastAccessStateTracker/Database/MongooseDatabase.ts
+++ src/FastAccessStateTracker/Database/MongooseDatabase.ts
@@ -1,54 +1,99 @@
 import { IDatabase } from "./IDatabase";
+
 import { FlowProject } from "../FlowLibrary/FlowProject";
 import { FlowUser } from "../FlowLibrary/FlowUser";
 import { FlowObject } from "../FlowLibrary/FlowObject";
+import { FlowClient } from "../FlowLibrary/FlowClient";
 
+import { ProjectOperations } from "../../commands/project"
+import { ClientOperations } from "../../commands/client"
+import { ObjectOperations } from "../../commands/object"
+import { UserOperations } from "../../commands/user"
 /**
  * Implementation of Mongoose Database
  */
 export class MongooseDatabase implements IDatabase
 {
-  CreateProject(projectToCreate: FlowProject): boolean {
-    throw new Error("Method not implemented.");
-  }
-  DeleteProject(projectToDelete: FlowProject): boolean {
-    throw new Error("Method not implemented.");
-  }
-  UpdateProject(projectToUpdate: FlowProject): boolean {
-    throw new Error("Method not implemented.");
-  }
-  GetProject(projectId: number): FlowProject {
-    throw new Error("Method not implemented.");
-  }
-  CreateUser(userToCreate: FlowUser): boolean {
-    throw new Error("Method not implemented.");
-  }
-  DeleteUser(userToDelete: FlowUser): boolean {
-    throw new Error("Method not implemented.");
-  }
-  UpdateUser(userToUpdate: FlowUser): boolean {
-    throw new Error("Method not implemented.");
-  }
-  GetUser(UserId: number): FlowUser {
-    throw new Error("Method not implemented.");
-  }
-  CreateObject(objectToCreate: FlowObject): boolean {
-    throw new Error("Method not implemented.");
-  }
-  DeleteObject(objectToDelete: FlowObject): boolean {
-    throw new Error("Method not implemented.");
-  }
-  UpdateObject(objectToUpdate: FlowObject): boolean {
-    throw new Error("Method not implemented.");
-  }
-  GetObject(ObjectId: number): FlowObject {
-    throw new Error("Method not implemented.");
-  }
   private _URL: string;
 
   constructor(url : string)
   {
     this._URL = url;
+  }
+
+  // Project functions
+  async CreateProject(projectToCreate: FlowProject) {
+    var project = await ProjectOperations.createProject(projectToCreate);
+    await project.save();
+
+    return project
+  }
+
+  async DeleteProject(projectToDelete: FlowProject): Promise<void> {
+    await ProjectOperations.deleteProject(projectToDelete);
+    return;
+  }
+
+  async UpdateProject(projectToUpdate: FlowProject): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  async GetProject(projectId: number): Promise<FlowProject> {
+    throw new Error("Method not implemented.");
+  }
+
+  // User functions
+
+  // This returns type any because mongoose is written in javascript so I literally don't know the return type here
+  async CreateUser(userToCreate: FlowUser): Promise<void> {
+    var newUser = await UserOperations.createUser(userToCreate);
+  
+    await newUser.save();
+  }
+
+  async DeleteUser(userToDelete: FlowUser): Promise<void> {
+    var User = await UserOperations.findUser(userToDelete);
+    var clientArray = User.Clients;
+
+    for(var arr in clientArray){
+
+        await ClientOperations.deleteClient(arr);
+
+    }
+
+    await UserOperations.deleteUser(userToDelete);
+  }
+
+  async UpdateUser(userToUpdate: FlowUser): Promise<void> {
+    await UserOperations.updateUser(userToUpdate);
+  }
+
+  async GetUser(UserId: number): Promise<FlowUser> {
+    let userToReturn = await UserOperations.findUser({username: UserId})
+    return new FlowUser(userToReturn) 
+  }
+
+  // Object functions
+  async CreateObject(objectToCreate: FlowObject, objectProject: FlowProject): Promise<Object> {
+    var object = await ObjectOperations.createObject(objectToCreate);
+    var project = await ProjectOperations.findProject(objectProject);
+
+    project.objs.push(object._id);
+    await project.save();
+
+    return object;
+  }
+
+  async DeleteObject(projectToDelete:FlowProject, projectObject:FlowObject): Promise<void> {
+    var project = await ProjectOperations.findProject(project);
+
+    await project.save();
+    await ObjectOperations.deleteObject(projectObject);
+  }
+  async UpdateObject(objectToUpdate: FlowObject): Promise<void> {
+    await ObjectOperations.updateObject(objectToUpdate);
+  }
+  async GetObject(ObjectId: number): Promise<FlowObject> {
+    return new FlowObject(await ObjectOperations.findObject(ObjectId))
   }
 
 }
