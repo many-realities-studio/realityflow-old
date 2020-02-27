e954fc5ca93e9801e213e24871682703ee1bca67
Yash <bhosaley5@gmail.com>
2020-02-27 02:23:22 -0500

    Added function statuses, clients are now fully just strings

--- src/FastAccessStateTracker/Room.ts
+++ src/FastAccessStateTracker/Room.ts
@@ -27,27 +27,36 @@ export class Room
     // ConnectionManager.SendMessage(data, this._UsersCurrentlyInTheRoom);    
   }
 
+  // TODO: finished: no tested: no
+  // I think I want to have strings passed to these
+ 
   /**
    * Adds the desired user to the list of users in the room
    * @param userJoiningTheRoom the user that will be joining the room
    * @param clientJoiningTheRoom the client that the user is using
    */
-  public JoinRoom(userJoiningTheRoom: FlowUser, clientJoiningTheRoom: FlowClient) : void
+  public JoinRoom(userJoiningTheRoom: FlowUser, clientJoiningTheRoom: String) : void
   {
-    let user = this._UsersCurrentlyInTheRoom.find(element => element.Id == userJoiningTheRoom.Id)
+    let user = this._UsersCurrentlyInTheRoom.find(element => element.Username == userJoiningTheRoom.Username)
 
     //user is not already in this room
     if(user == undefined){
       this._UsersCurrentlyInTheRoom.push(userJoiningTheRoom);
     }
-    // user is already in this room
-    else{
-      user.ActiveClients.push(clientJoiningTheRoom)
-    }
+    
+    user.ActiveClients.push(clientJoiningTheRoom)
+    
   }
 
-  public LeaveRoom(userLeavingTheRoom: FlowUser, clientLeavingTheRoom: FlowClient){
-    let userIndex = this._UsersCurrentlyInTheRoom.findIndex(element => element.Id == userLeavingTheRoom.Id)
+  // TODO: finished: yes? tested: no
+  /**
+   * removes a client from a room - if that client was the only one using those credentials,
+   * the room stops keeping track of that username
+   * @param UserName username of the client leaving the room
+   * @param ClientId client id of the client leaving the room
+   */
+  public LeaveRoom(UserName: String, ClientId: String){
+    let userIndex = this._UsersCurrentlyInTheRoom.findIndex(element => element.Username == UserName)
 
     //user is already not in this room
     if(userIndex == -1){
@@ -55,7 +64,7 @@ export class Room
     }
     // user is already in this room
     else{
-      let clientIndex = this._UsersCurrentlyInTheRoom[userIndex].ActiveClients.findIndex(element => element.Id == clientLeavingTheRoom.Id)
+      let clientIndex = this._UsersCurrentlyInTheRoom[userIndex].ActiveClients.findIndex(element => element == ClientId)
       this._UsersCurrentlyInTheRoom[userIndex].ActiveClients.splice(clientIndex, 1)
       
       //if the user doesn't have any more active clients, then remove user from the room
@@ -66,6 +75,18 @@ export class Room
 
   }
 
+  // TODO: finished: yes? tested: no
+  /**
+   * returns whether or not any client using this given username is in here
+   * @param username username to search for
+   */
+  public hasUser(username: String){
+    let retval = (this._UsersCurrentlyInTheRoom.find(element => element.Username == username) != undefined)
+    return retval
+  }
+
+
+  // TODO: finished: yes tested: no
   /**
    * Gets the room code of this room
    */
@@ -74,7 +95,7 @@ export class Room
     return this._CurrentProjectId;
   }
 
-
+  // TODO: finished: yes tested: no
   /**
    * Gets the project that is currently being used by the project
    * Calls to database, use in Async manner
