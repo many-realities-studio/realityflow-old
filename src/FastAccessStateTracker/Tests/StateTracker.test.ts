cbe64156882cd33e6b9950b5a8b698e1586cd056
Yash <bhosaley5@gmail.com>
2020-02-28 13:56:05 -0500

    working on tests

--- src/FastAccessStateTracker/Tests/StateTracker.test.ts
+++ src/FastAccessStateTracker/Tests/StateTracker.test.ts
@@ -9,38 +9,43 @@ jest.mock('../Database/MongooseDatabase')
 
 describe("User",() => {
     it("can be created", async () => {
-        const testUser = new FlowUser({
+        const testUser = {
             Username: "YashStateTracker",
             Password: "StateTrackerYash"
-        })
+        }
         
-        const mongooseUserCreateMock = jest.fn(async (userToCreate: FlowUser) => {}) 
+        const mongooseUserCreateMock = jest.fn(async (userName:string, Password: string) => {}) 
 
         MongooseDatabase.CreateUser = mongooseUserCreateMock
 
         //act
-        await StateTracker.CreateUser(testUser)
+        await StateTracker.CreateUser(testUser.Username, testUser.Password)
 
         //assert
-        expect(mongooseUserCreateMock).toHaveBeenCalledWith(testUser)
+        expect(mongooseUserCreateMock).toHaveBeenCalledWith(testUser.Username, testUser.Password)
     });
 
     it("can be deleted", async () =>{
-        const testUser = new FlowUser({
+        const testUser = {
             Username: "YashStateTracker",
             Password: "StateTrackerYash"
-        })
+        }
 
-        const mongooseUserDeleteMock = jest.fn(async (userToDelete: FlowUser) => {})
+        const mongooseUserDeleteMock = jest.fn(async (userToDelete: string) => {})
         MongooseDatabase.DeleteUser = mongooseUserDeleteMock
 
-        await StateTracker.DeleteUser(testUser)
+        await StateTracker.DeleteUser(testUser.Username, testUser.Password)
 
-        expect(mongooseUserDeleteMock).toHaveBeenCalledWith(FlowUser)
+        expect(mongooseUserDeleteMock).toHaveBeenCalledWith(testUser.Username)
     });
 
     it("can be logged in", () => {
+        const testUser = {
+            Username: "yash",
+            Password: "yashsPassword"
+        }
 
+        
     });
 
     it("can be logged out", ()=>{
