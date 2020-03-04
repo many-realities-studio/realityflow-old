8167005f101662afe16494c08f963077b63006ad
Yash <bhosaley5@gmail.com>
2020-03-04 11:25:30 -0500

    ended up realizing that I can just use JSON.stringify for the payload

--- src/FastAccessStateTracker/Messages/MessageBuilder.ts
+++ src/FastAccessStateTracker/Messages/MessageBuilder.ts
@@ -1,26 +1,11 @@
-import { IStringable } from "../FlowLibrary/IStringable";
 
 export class MessageBuilder
 {
   
-  public static CreateMessage(...param : any[]) : string
+  public static CreateMessage( payload : any, clients: string[]) : [string, Array<string>]
   {
-    let stringArray = param.map(function(currentVal: any) {
-      return currentVal.toString();
-    });
+    let returnPayload = JSON.stringify(payload)
     
-    return stringArray.reduce( function(previousVal: any, currentVal: any) {
-      return previousVal + "\n" + currentVal;
-    });
-  }
-
-  public static SuccessMessage(operation: string) : string
-  {
-    return operation + " was successful";
-  }
-
-  public static FailureMessage(operation: string) : string
-  {
-    return operation + " was a failure";
+    return [returnPayload, clients]
   }
 }
