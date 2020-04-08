577a6c3098029ab84e5af4f6aef89ff9ce423cc1
Yash <bhosaley5@gmail.com>
2020-04-08 15:56:31 -0400

    null check for action

--- src/FastAccessStateTracker/FlowLibrary/Action.ts
+++ src/FastAccessStateTracker/FlowLibrary/Action.ts
@@ -6,6 +6,8 @@ export class Action{
     ActionParameters: any;
 
     constructor(data: any) {
+        if(data == null)
+            return null;
         this.ActionId = data.ActionId;
         this.ActionType = data.ActionType;
         this.ActionParameters = data.ActionParameters; 
