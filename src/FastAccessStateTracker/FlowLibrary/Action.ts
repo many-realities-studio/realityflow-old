15c67b788449481d0f903d3df8945d14fa575653
Yash <bhosaley5@gmail.com>
2020-04-08 16:40:42 -0400

    Revert "null check for action"
    This reverts commit 577a6c3098029ab84e5af4f6aef89ff9ce423cc1.

--- src/FastAccessStateTracker/FlowLibrary/Action.ts
+++ src/FastAccessStateTracker/FlowLibrary/Action.ts
@@ -6,8 +6,6 @@ export class Action{
     ActionParameters: any;
 
     constructor(data: any) {
-        if(data == null)
-            return null;
         this.ActionId = data.ActionId;
         this.ActionType = data.ActionType;
         this.ActionParameters = data.ActionParameters; 
