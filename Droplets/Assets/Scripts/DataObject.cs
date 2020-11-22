0e09a1475925b39e5f634a2d03d230460c3231c7
VictorSolal <46535199+VictorSolal@users.noreply.github.com>
2020-11-22 13:49:58 -0500

    Visual scripting fork initialization (#9)
    * Visual scripting fork initialization
    
    * Removed Unity Bolt from Visual Scripting fork

---
+++ Droplets/Assets/Scripts/DataObject.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class DataObject : GUIDraggableObject
+// This class just has the capability of being dragged in GUI - it could be any type of generic data class
+{
+	public string d_Name;  // this can be both var & logic
+	// public string m_Type;  // send to variable node
+	// private int m_Value;  // send over to variablenode
+	public float width = 150.0f;
+	public float height = 150.0f;
+	public bool notDeleted = true;
+
+/* 	public DataObject (string name, string type, int value, Vector2 position) : base (position)
+	{
+		m_Name = name;
+		m_Value = value;
+		m_Type = type;
+	} */
+
+	public DataObject (string name, Vector2 position) : base (position)
+	{
+		d_Name = name;
+	}
+
+	public virtual void OnGUI(){
+		Rect drawRect = new Rect (m_Position.x, m_Position.y, width, height), dragRect;
+		GUILayout.BeginArea (drawRect, GUI.skin.GetStyle ("Box"));
+			GUILayout.Label (d_Name, GUI.skin.GetStyle ("Box"), GUILayout.ExpandWidth (true));
+			dragRect = GUILayoutUtility.GetLastRect ();
+			dragRect = new Rect (dragRect.x + m_Position.x, dragRect.y + m_Position.y, dragRect.width, dragRect.height);
+
+			if (GUILayout.Button ("Debug")){
+				Debug.Log("Name: "+ d_Name);
+			}
+			else if (GUILayout.Button ("Delete"))
+			{
+				notDeleted = false;
+				// TODO: Figure out how to remove from list witout breaking for each loop
+				//VisualScriptingWindow.m_Data.Remove(this);
+			}
+		GUILayout.EndArea ();
+
+		Drag (dragRect);
+	}
+	
+	public string toString(){
+		return d_Name;
+	}
+}
---
+++ Droplets/Assets/Scripts/GUIDraggableObject.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIDraggableObject
+{
+	public Vector2 m_Position;
+	private Vector2 m_DragStart;
+	private bool m_Dragging;
+
+	public GUIDraggableObject (Vector2 position)
+	{
+		m_Position = position;
+	}
+
+	public bool Dragging
+	{
+		get
+		{
+			return m_Dragging;
+		}
+	}
+
+	public Vector2 Position
+	{
+		get
+		{
+			return m_Position;
+		}
+
+		set
+		{
+			m_Position = value;
+		}
+	}
+
+	public void Drag (Rect draggingRect)
+	{
+		if (Event.current.type == EventType.MouseUp)
+		{
+			m_Dragging = false;
+		}
+		else if (Event.current.type == EventType.MouseDown && draggingRect.Contains (Event.current.mousePosition))
+		{
+			m_Dragging = true;
+			m_DragStart = Event.current.mousePosition - m_Position;
+			Event.current.Use();
+		}
+
+		if (m_Dragging)
+		{
+			m_Position = Event.current.mousePosition - m_DragStart;
+		}
+	}
+}
