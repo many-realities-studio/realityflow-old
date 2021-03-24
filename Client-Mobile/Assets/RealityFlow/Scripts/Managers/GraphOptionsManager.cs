6b7213f9defbd95ebd6e95f1a29f5555b876e3f0
Morgan-Wilkinson <46545893+Morgan-Wilkinson@users.noreply.github.com>
2021-03-23 20:12:58 -0400

    Updates to Mobile App.
    Graph View now works except for the function DoProcessing. I did not find a cause as to why it does not work.

--- Client-Mobile/Assets/RealityFlow/Scripts/Managers/GraphOptionsManager.cs
+++ Client-Mobile/Assets/RealityFlow/Scripts/Managers/GraphOptionsManager.cs
@@ -5,10 +5,17 @@ using TMPro;
 
 public class GraphOptionsManager : MonoBehaviour
 {
+    bool startDetectingTouch = false;
+    Vector2 nodePosition;
+    public GameObject nodePlacementPopup;
     TMP_Dropdown addNodeTypeMenu;
     TMP_Dropdown graphCommandsMenu;
 
     public RealityFlowGraphView realityFlowGraphView;
+    
+    // The reason this is being used is because in RealityFlowGraphView the node position is multiplied by this hardcoded
+    // canvas dimensions. So in order to compansate for this I am dividing by the canvas hardcoded dimensions.
+    public Vector2 canvasDimensions = new Vector2(2000, 1080); // FOR NOW, dont have these hardcoded in final demo
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +28,31 @@ public class GraphOptionsManager : MonoBehaviour
         });
 
         addNodeTypeMenu.onValueChanged.AddListener(delegate {
-            NodeCreationSwitch();
+            StartCoroutine("NodeCreationSwitch");
         });
+    }
+
+    void Update()
+    {
+        if(startDetectingTouch)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                 Vector2 mousePos = Input.mousePosition;
+                 Debug.Log(mousePos);
+                 //nodePosition = new Vector2(mousePos.x, mousePos.y); // This is the correct form. See node by canvasDimension declaration 
+                 nodePosition = new Vector2(mousePos.x / canvasDimensions.x, mousePos.y / canvasDimensions.y);
+                 startDetectingTouch = false;
+            }
+        }
     }
 
     public void GraphOptionsSwitch()
     {
+        if(graphCommandsMenu.value == 0)
+        {
+            return;
+        }
         switch(graphCommandsMenu.options[graphCommandsMenu.value].text.ToString())
 		{
 			case "Add Parameter":
@@ -56,37 +82,66 @@ public class GraphOptionsManager : MonoBehaviour
 			default:
 				Debug.Log("No valid option selected.");
 				break; 
-		}		
+		}
+        graphCommandsMenu.value = 0;
     }
 
-    public void NodeCreationSwitch()
+    private IEnumerator NodeCreationSwitch()
     {
-        switch(addNodeTypeMenu.options[addNodeTypeMenu.value].text.ToString())
-		{
-            case "Text Node":
-                realityFlowGraphView.AddNodeCommand("TextNode");
-                Debug.Log("Text Node created from GraphOptionsSwitch");
-				break;
-            case "Float Node":
-                realityFlowGraphView.AddNodeCommand("FloatNode");
-                Debug.Log("Float Node created from GraphOptionsSwitch");
-				break;
-            case "Int Node":
-                realityFlowGraphView.AddNodeCommand("IntNode");
-                Debug.Log("Int Node created from GraphOptionsSwitch");
-				break;
-            case "Bool Node":
-                realityFlowGraphView.AddNodeCommand("BoolNode");
-                Debug.Log("Bool Node created from GraphOptionsSwitch");
-				break;
-            case "Conditional Node":
-                realityFlowGraphView.AddNodeCommand("ConditionalNode");
-                Debug.Log("Conditional Node created from GraphOptionsSwitch");
-				break;
-            default:
-				Debug.Log("No valid node option selected.");
-				break;
+        // Turns on node placement popup
+        nodePlacementPopup.SetActive(!nodePlacementPopup.activeInHierarchy);
+        Debug.Log(startDetectingTouch);
+        
+        while(nodePlacementPopup.activeInHierarchy)
+        {
+            yield return null;
+        }
+
+        if(!nodePlacementPopup.activeInHierarchy)
+        {
+            startDetectingTouch = true;
+            while(startDetectingTouch)
+            {
+                yield return null;
+            }
+            Debug.Log("Yak " + nodePosition);   
+            realityFlowGraphView.SetNewNodeLocation(nodePosition);
+        
+            switch(addNodeTypeMenu.options[addNodeTypeMenu.value].text.ToString())
+            {
+                case "Text Node":
+                    realityFlowGraphView.AddNodeCommand("TextNode");
+                    Debug.Log("Text Node created from GraphOptionsSwitch");
+                    break;
+                case "Float Node":
+                    realityFlowGraphView.AddNodeCommand("FloatNode");
+                    Debug.Log("Float Node created from GraphOptionsSwitch");
+                    break;
+                case "Int Node":
+                    realityFlowGraphView.AddNodeCommand("IntNode");
+                    Debug.Log("Int Node created from GraphOptionsSwitch");
+                    break;
+                case "Bool Node":
+                    realityFlowGraphView.AddNodeCommand("BoolNode");
+                    Debug.Log("Bool Node created from GraphOptionsSwitch");
+                    break;
+                case "Conditional Node":
+                    realityFlowGraphView.AddNodeCommand("ConditionalNode");
+                    Debug.Log("Conditional Node created from GraphOptionsSwitch");
+                    break;
+                default:
+                    Debug.Log("No valid node option selected.");
+                    break;
+            }
         }
     }
-    
+
+    private IEnumerator WaitForPosition()
+    {
+        while(nodePosition == null)
+        {
+            yield return null;
+        }
+        Debug.Log(nodePosition);    
+    }
 }
--- Client-Mobile/Assets/RealityFlow/Scripts/UI/MovePanel.cs
+++ Client-Mobile/Assets/RealityFlow/Scripts/UI/MovePanel.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MovePanel : MonoBehaviour {
 
+    public TMP_Dropdown panelMenu;
+    public GameObject objectInteractionItems;
+    public GameObject[] panelList;
     public const float PANEL_OFFSET = 737f;
+
+    void Start()
+    {
+        panelMenu.onValueChanged.AddListener(delegate {
+            TogglePanel();
+        });
+
+    }
 
     // Moves a panel a given number of screen-dependent pixels in a given direction
     // panel = -1 refers to the scene (left) panel
     // panel = 1 refers to the project (right) panel
+    /*
     public void Move (int panel)
     {
         if ( (GetComponent<RectTransform>().offsetMin.x == PANEL_OFFSET * panel) || 
             (GetComponent<RectTransform>().offsetMax.x == PANEL_OFFSET * panel) )
         {
+            TogglePanel();
             // Set left, bottom RectTransform properties of panel
             GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
             // Set right, top RectTransform properties of panel
@@ -23,6 +37,7 @@ public class MovePanel : MonoBehaviour {
         }
         else
         {
+            TogglePanel();
             // Set left, bottom RectTransform properties of panel
             GetComponent<RectTransform>().offsetMin = new Vector2(GetComponent<RectTransform>().offsetMin.x + PANEL_OFFSET * panel, 0);
             // Set right, top RectTransform properties of panel
@@ -30,5 +45,27 @@ public class MovePanel : MonoBehaviour {
             // Send panel to background
             GetComponent<RectTransform>().SetAsFirstSibling();
         }
+    }
+    */
+
+    public void TogglePanel()
+    {
+        objectInteractionItems.SetActive(false);
+
+        for(int i = 0; i < panelList.Length; i++)
+        {
+            if(panelList[i].name == panelMenu.options[panelMenu.value].text.ToString())
+            {
+                panelList[i].SetActive(true);
+            }
+        }
+        panelMenu.value = 0;
+    }
+
+    public void closePanel()
+    {
+        transform.parent.gameObject.SetActive(false);
+        objectInteractionItems.SetActive(true);
+
     }
 }
