253592f7ad22e736a78048f3f632f9de8ec7e93f
Iminance <owenmak4@gmail.com>
2021-04-15 16:00:01 -0400

    Imported Dissonance PUN demo for research and testing

---
+++ UnityPlugin/Assets/Dissonance/Integrations/PhotonUnityNetworking2/Demo/PlayerSpawner.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Dissonance.Integrations.PhotonUnityNetworking2.Demo
+{
+    public class PlayerSpawner
+        : MonoBehaviour
+    {
+        [UsedImplicitly] public GameObject ObjectToSpawn;
+
+        [UsedImplicitly] private void Start()
+        {
+            var rand = new System.Random();
+            var pos = new Vector3(rand.Next(-15, 15), 0, rand.Next(-15, 15));
+
+            PhotonNetwork.Instantiate(ObjectToSpawn.name, pos, Quaternion.identity, 0);
+
+            Destroy(gameObject);
+        }
+    }
+}
