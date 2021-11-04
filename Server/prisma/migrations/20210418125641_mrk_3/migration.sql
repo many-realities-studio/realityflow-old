60249eb74aebda91d0db1fe38609220e3b58e111
lucidbard <john@lucidbard.com>
2021-11-04 11:31:15 -0400

    Updating server as submodule

--- Server/prisma/migrations/20210418125641_mrk_3/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[Username]` on the table `user` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "user.Username_unique" ON "user"("Username");
